42e38d386688b70891ca3673948794f8eedef5c1
Ameya Dighole <Adighole@realgy.com>
2021-09-02 20:29:03 +0530

    Added post put and delete methods

--- user-api/Controllers/UserController.cs
+++ user-api/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace user_api.Controllers
 {
@@ -28,7 +29,44 @@ namespace user_api.Controllers
         [HttpPost]
         public IActionResult Post(User model)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+                return BadRequest("invalid parameters");
+
+            _context.Users.Add(model);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { id = model.Id }, model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, User user)
+        {
+            if (id != user.Id)
+                return BadRequest("Id mismatch");
+            if (!ModelState.IsValid)
+                return BadRequest("invalid parameters");
+
+            _context.Attach(user);
+            _context.Entry(user).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+
+            var userToDelete = _context.Users.Where(user => user.Id == id).FirstOrDefault();
+
+            if (userToDelete == null)
+            {
+                return NotFound($"User with Id = {id} not found");
+            }
+
+            _context.Users.Remove(userToDelete);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
